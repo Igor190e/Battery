bda71427c3bef66479a164ab6035cb35e74e8e0a
Igor <igor190e@mail.ru>
2018-12-02 13:55:07 +0300

    Interface and Find Arduino on COM port

--- Battery/MainWindow.xaml.cs
+++ Battery/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.IO.Ports;
 
 namespace Battery
 {
@@ -20,9 +22,57 @@ namespace Battery
     /// </summary>
     public partial class MainWindow : Window
     {
+        string arduinoCom = "";
+        byte InByte;
+        Int16 InInt;
+        string InString;
+
+
         public MainWindow()
         {
+
             InitializeComponent();
+
+        }
+
+        private void SearchArduinoCom_Click(object sender, RoutedEventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            SerialPort Com;
+            
+            string tmp = "";
+            foreach (string a in ports)
+            {
+                Com = new SerialPort(a, 9600);
+                Com.Open();
+                Com.ReadTimeout=500;
+                System.Threading.Thread.Sleep(100);
+                try
+                {
+                    tmp = Com.ReadLine();
+                   
+                    if (Com.ReadLine().Trim() == "ARDUINO")
+                    {
+                        arduinoCom = a;
+                        Com.WriteLine("INIT");
+                        ArduinoCom.Content = arduinoCom;                       
+                        Com.Close();
+                        break;
+                    }
+                }
+                catch { Com.Close(); }
+             
+            }  
+        }
+
+        private void ReadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ByteRadioButton.IsEnabled == true)
+            { }
+            else if (Int16RadioButton.IsEnabled == true)
+            { }
+            else if (StringRadioButton.IsEnabled == true)
+            { }
         }
     }
 }

a5d6089d36c5bbab4bb3321370ee3aeb910964c4
Igor190e <igor190e@mail.ru>
2018-12-14 23:14:03 +0300

    work with search arduino

--- Battery/MainWindow.xaml.cs
+++ Battery/MainWindow.xaml.cs
@@ -22,29 +22,17 @@ namespace Battery
     /// </summary>
     public partial class MainWindow : Window
     {
-        string arduinoCom = "COM3";
-        byte InByte;
+        string arduinoCom; // "COM3";
+        bool find = false;
         int InInt;
         string InString;
-        SerialPort Com;//=new SerialPort("COM3", 9600);
-        
+        SerialPort Com; //=new SerialPort("COM3", 9600);        
 
         public MainWindow()
         {
-            Com= new SerialPort("COM3", 9600);
-           
-            System.Threading.Thread.Sleep(1000);
-
-         
-
-            Com.Open();
-
-
-
-            Com.ReadTimeout = 3000;
             InitializeComponent();
-
         }
+
 
         private void SearchArduinoCom_Click(object sender, RoutedEventArgs e)
         {
@@ -55,8 +43,9 @@ namespace Battery
             foreach (string a in ports)
             {
                 Com = new SerialPort(a, 9600);
+                Com.ReadTimeout = 3000;
                 Com.Open();
-                Com.ReadTimeout=3000;
+
                 System.Threading.Thread.Sleep(1000);
                 try
                 {
@@ -65,13 +54,21 @@ namespace Battery
                     if (tmp.Trim() == "ARDUINO")
                     {
                         arduinoCom = a;
-                        Com.WriteLine("INIT");
-                        ArduinoCom.Content = arduinoCom;                       
-                       Com.Close();
-                        break;
+                        find = true;
+                        Com.Write("INIT");                        
+                        ArduinoCom.Content = arduinoCom;
+                        
                     }
                 }
-                catch { Com.Close(); }
+                catch
+                {
+                    if (find == false)
+                    { Com.Close(); }
+                }
+                if (find == true)
+                {
+                    break;
+                }
              
             }  
         }
@@ -86,17 +83,23 @@ namespace Battery
                 //Com.ReadTimeout = 3000;
                 System.Threading.Thread.Sleep(500);
                 Com.Write("ReadByte");
-                System.Threading.Thread.Sleep(500);
-                try
-                {
-                    InString = Com.ReadLine();
-                }
-                catch { }
+
+                while (Com.BytesToRead <= 0)
+                    System.Threading.Thread.Sleep(1000);
+
+                InInt = int.Parse(Com.ReadLine());
+
+                //System.Threading.Thread.Sleep(500);
+                //try
+                //{
+                //    InString = Com.ReadLine();
+                //}
+                //catch { }
                 //InInt = int.Parse(InString);
-                HexData.Text = InString;
-                //HexData.Text = Convert.ToString(InInt, 16);
-                //DecData.Text = Convert.ToString(InInt);
-                //BinData.Text = Convert.ToString(InInt, 2);
+                //HexData.Text = InString;
+                HexData.Text = Convert.ToString(InInt, 16);
+                DecData.Text = Convert.ToString(InInt);
+                BinData.Text = Convert.ToString(InInt, 2);
                 //Com.Close();
             }
             else if (Int16RadioButton.IsChecked == true)
@@ -106,7 +109,8 @@ namespace Battery
                 //Com.ReadTimeout = 3000;
                 System.Threading.Thread.Sleep(1000);
                 Com.Write("ReadInt16");
-                System.Threading.Thread.Sleep(10000);
+                while (Com.BytesToRead <= 0)
+                    System.Threading.Thread.Sleep(1000);
                 //try
                 //{
                 //    InString = Com.ReadLine();
@@ -128,18 +132,22 @@ namespace Battery
             {
                 InString = "";
                 int buf;
-                byte[] bt=new byte[5];
+
+                //byte[] bt=new byte[5];
 
                 
                 Com.Write("ReadString");
-
                 
+                //System.Threading.Thread.Sleep(1000);
+                //Com.Write(StringLength.Text);
+                //bt = new byte[int.Parse(StringLength.Text)];
+
                 while (Com.BytesToRead<=0)
                 System.Threading.Thread.Sleep(1000);
 
 
-                byte[] b = new byte[5];
-                Com.Read(b, 0, 5);
+                byte[] b = new byte[int.Parse(StringLength.Text)];
+                Com.Read(b, 0, int.Parse(StringLength.Text));
                 string s = Encoding.ASCII.GetString(b);
 
                 InString = s;// asciiEncoding.GetString(bt);
@@ -160,10 +168,11 @@ namespace Battery
             //Com.ReadTimeout = 3000;
             //System.Threading.Thread.Sleep(1000);
             Com.Write("WriteByte");
-            System.Threading.Thread.Sleep(1000);
-            int buf = Convert.ToInt32(SendTextByte.Text,16);
+            System.Threading.Thread.Sleep(500);
+            int buf = int.Parse(SendTextByte.Text, System.Globalization.NumberStyles.HexNumber);
+            //Convert.ToInt32(SendTextByte.Text,16);
             Com.Write(Convert.ToString(buf));
-            System.Threading.Thread.Sleep(1000);
+            System.Threading.Thread.Sleep(500);
             //Com.Close();
         }
 
